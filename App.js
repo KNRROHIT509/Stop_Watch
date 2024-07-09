b1e78e7270cb20f786079e3f6f4e434f40bbb6a0
KNRROHIT509 <111121690+KNRROHIT509@users.noreply.github.com>
2024-07-09 11:03:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Stopwatch from './components/Stopwatch/Stopwatch';
+import './App.css';
+
+function App() {
+  return (
+    <div className ="container">
+    <div className="App">
+      <Stopwatch />
+    </div>
+    </div>
+  );
+}
+
+export default App;
